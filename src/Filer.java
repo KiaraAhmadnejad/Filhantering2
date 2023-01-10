000d21999d710bb9092718e1d257d53476c7af90
Kiara <ahmadnejadkiara@gmail.com>
2023-01-10 21:37:42 +0100

    påbörjat på filhanteringen

---
+++ src/Filer.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Filer {
+
+    Scanner myScanner = new Scanner(System.in);
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // koden i main gör så att koden och information från filer kopplas ihop till koden i Main
+
+        Filer myFiler = new filer();
+        myFiler.start();
+    }
+}
